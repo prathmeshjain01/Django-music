a7348b49996a07d9bb0217b455183255447dda07
prathamjain <55786589+prathmeshjain01@users.noreply.github.com>
2021-06-10 14:37:49 +0530

    domain added

--- music/settings.py
+++ music/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'g-qk^&kf9i_(_s1(df!!)&dy$j6w81%vdhozw5knnvvb+6=(&q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1',"deep-music.herokuapp.com"]
 
 
 # Application definition
